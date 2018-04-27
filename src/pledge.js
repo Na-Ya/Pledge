bad569bf9f8cdab124fb52ef795036cb1df1f362
Plamen Dzhelepov <plamen.gj@gmail.com>
2018-04-27 15:56:26 -0400

    chap 2, spec 2

--- src/pledge.js
+++ src/pledge.js
@@ -9,6 +9,7 @@ function $Promise(executor) {
 	this._state = 'pending';
 	this._value;
 
+	this._handlerGroups = []
 	this._internalResolve = function(value) {
 		if (!this._value && this._state === 'pending') {
 			this._value = value;
@@ -34,7 +35,35 @@ function $Promise(executor) {
 			this._internalReject(reject);
 		}
 	);
+
+	this.then = (successCb, errorCb)=>{
+		if (typeof successCb !== 'function'){
+			successCb = null;
+		}
+		if (typeof errorCb !== 'function'){
+			errorCb = null;
+		}
+		this._handlerGroups.push({
+			successCb: successCb,
+			errorCb: errorCb
+		});
+		this._callHandlers()
+		// console.log('5: ', this._handleGroups[0])
+		// console.log(this._handleGroups[0].errorCb);
+	};
 }
+
+$Promise.prototype._callHandlers = ()=>{
+	if(this._state === 'fulfilled'){
+		let current = this._handlerGroups.shift()
+		current.successCb()
+	}
+	if(this._state === 'rejected'){
+		let current = this._handlerGroups.shift()
+		current.errorCb()
+	}
+}
+
 
 /*-------------------------------------------------------
 The spec was designed to work with Test'Em, so we don't
